e6dfc681bbdf383024aba4a1a2166ba972ff1eb3
Igor <silent659@gmail.com>
2023-03-14 20:01:54 +0200

    home page responsive ui

--- src/main.js
+++ src/main.js
@@ -1,23 +1,42 @@
-import './style.css'
-import javascriptLogo from '../assets/svg/javascript.svg'
-import { setupCounter } from './js/counter.js'
+import "./style.css";
+import "./sass/range-mixin.sass";
+import "./sass/header.sass";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const _R = document.querySelectorAll("[type=range]"),
+  _W = _R.parentNode,
+  _O = _R.nextElementSibling;
 
-setupCounter(document.querySelector('#counter'))
+document.documentElement.classList.add("js");
+
+for (let i = 0; i < _R.length; i++) {
+  _R[i].addEventListener(
+    "input",
+    (e) => {
+      if (e.target.id == "money-range") {
+        e.currentTarget.style.setProperty("--val", +e.currentTarget.value);
+        e.currentTarget.nextElementSibling.value =
+          (e.currentTarget.value * 1000).toString().slice(0, 2) +
+          " " +
+          (e.currentTarget.value * 1000).toString().slice(2, 5) +
+          " ₮";
+        e.currentTarget.parentNode.style.setProperty("--val", +e.currentTarget.value);
+      }
+      if (e.target.id == "term-range") {
+        e.currentTarget.style.setProperty("--val", +e.currentTarget.value);
+        e.currentTarget.nextElementSibling.value = e.currentTarget.value + " дней";
+        e.currentTarget.parentNode.style.setProperty("--val", +e.currentTarget.value);
+      }
+    },
+    false
+  );
+}
+
+// _R.addEventListener(
+//   "input",
+//   (e) => {
+//     _R.style.setProperty("--val", +_R.value);
+//     _O.value = Number(_R.value) * 1000;
+//     _W.style.setProperty("--val", +_R.value);
+//   },
+//   false
+// );
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,24 @@
 import { defineConfig } from "vite";
 
+import handlebars from "vite-plugin-handlebars";
+import { resolve } from "path";
+
+function handlebarsOverride(options) {
+  const plugin = handlebars(options);
+  // Currently handleHotUpdate skips further processing, which bypasses
+  // postcss and in turn tailwind doesn't pick up file changes
+  delete plugin.handleHotUpdate;
+  return plugin;
+}
+
 export default defineConfig({
-  // ...
+  plugins: [
+    handlebarsOverride({
+      context: {
+        title: "Handlebars",
+      },
+      partialDirectory: resolve(__dirname, "./src/partials"),
+    }),
+    // handlebars({}),
+  ],
 });
