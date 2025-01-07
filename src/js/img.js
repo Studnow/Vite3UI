d1704bae3acfff8974c36512db9631a0d66c2c7b
Igor <silent659@gmail.com>
2025-01-07 08:25:16 +0200

    image optimization, path to context

---
+++ src/js/img.js
@@ -0,0 +1,11 @@
+// import imageTopWebp from "../../assets/img/Hero-bg.jpg";
+// import imageTopJpg from "/assets/img/waterfall.png";
+// import viteSvg from '../../public/vite.svg'
+
+// export const img = {
+  // top: {
+    // webp: imageTopWebp,
+    // img: imageTopJpg,
+    // svg: viteSvg
+  // },
+// };
